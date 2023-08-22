639ed0f8f804fcb500684e6f4248520002ea85ac
MustafaAkipek <101327731+MustafaAkipek@users.noreply.github.com>
2023-08-22 08:22:37 +0300

    variables

---
+++ variables.cs
@@ -0,0 +1,28 @@
+﻿// Variables cannot contain spaces
+// Variables cannot start with a number
+// Variables have case-lowercase sensitivity
+
+var firstName = "Mustafa";
+var lastName = "Akipek";
+var fullName = firstName + " " + lastName;
+var studentNo = 482;
+var studentId = "111111";
+var studentBirthYear = 2003;
+var studentAge = 2023 - studentBirthYear;
+var studentAddress = "Bursa Gemlik";
+
+Console.WriteLine(firstName);
+Console.WriteLine(lastName);
+Console.WriteLine(fullName);
+Console.WriteLine(studentNo);
+Console.WriteLine(studentId);
+Console.WriteLine(studentBirthYear);
+Console.WriteLine(studentAge);
+Console.WriteLine(studentAddress);
+
+var product1_price = 50;
+var product2_price = 60.5;
+var product3_price = 356.45;
+
+var sum = product1_price + product2_price + product3_price;
+Console.WriteLine(sum);
