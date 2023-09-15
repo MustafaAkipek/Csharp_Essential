3d8589e34ad790d69f0a7ee3319fd44d7bd078da
MustafaAkipek <101327731+MustafaAkipek@users.noreply.github.com>
2023-09-15 08:12:18 +0300

    loops

---
+++ loops.cs
@@ -0,0 +1,37 @@
+﻿// For Loop
+
+    // for(var i = 1; i < 10; i++) {
+    //     Console.WriteLine(i);
+    // }
+
+// While Loop
+
+    // var i = 0;
+
+    // while(i < 10) {
+    //     Console.WriteLine(i);
+    //     i++;
+
+    //     if(i == 8)
+    //         break;
+
+    //     if(i == 5)
+    //         continue;
+    // }
+
+// Do-While Loop
+
+    // int i = 1;
+
+    // do {
+    //     Console.WriteLine(i);
+    //     i++;
+    // } while(false);
+
+// Foreach Loop
+
+string name = "Mustafa";
+
+foreach(var letter in name) {
+    Console.WriteLine(letter);
+}
